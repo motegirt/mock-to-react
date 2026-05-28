30457c276d553d95a67f6fa496707ae2126073f0
motegirt <motegirt@noar.co.jp>
2026-05-28 18:56:23 +0900

    React用にMockitoの課題から作り直し

--- src/main/java/com/example/demo/application/service/ProductService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.application.service;
-import com.example.demo.domain.model.ProductData;
-public interface ProductService {
-	public ProductData getProductInfo(String productId);
-}
--- src/main/java/com/example/demo/application/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.application.service;
-import com.example.demo.domain.model.UserData;
-public interface UserService {
-	public UserData getUserInfo();
-}
